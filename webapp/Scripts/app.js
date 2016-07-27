40dd949640b0c1bfb210c6f316cefc18a21c4f34
Varun Goel <varun.goel@optimusinfo.com>
2016-07-27 22:08:00 +0530

    Added Grid

--- webapp/Scripts/app.js
+++ webapp/Scripts/app.js
@@ -2,19 +2,5 @@
 
 
 angular.module('app', [
-    'ui.router'
+    //'ui.router'
 ])
-
-
-angular.module('app').config(function ($stateProvider) {
-
-    $stateProvider
-        .state('app.form', {
-            data: {
-                title: 'Forms'
-            }
-        });
-
- 
-
-});
